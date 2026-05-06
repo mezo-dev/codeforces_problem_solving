cb2882e20631794a501e90fad42246c331dbaae0
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-07 00:24:36 +0300

    feat: remove smallest

---
+++ 145.py
@@ -0,0 +1,24 @@
+
+
+
+
+def remove_smallest() -> None:
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+
+        a.sort()
+
+        possible = True
+        for i in range(n - 1):
+            if a[i + 1] - a[i] > 1:
+                possible = False
+                break
+        if possible:
+            print("Yes")
+        else:
+            print("No")
+
+remove_smallest()
