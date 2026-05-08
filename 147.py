9ccb504e3c6e1181bf60fee2159af74f813e55ef
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-09 01:52:28 +0300

    feat: sovled

---
+++ 147.py
@@ -0,0 +1,18 @@
+
+
+
+
+def solve():
+    t = int(input())
+
+    for _ in range(t):
+        n, k = map(int, input().split())
+        n_arr = set(map(int, input().split()))
+
+        if k in n_arr:
+            print("Yes")
+        else: 
+            print('No')
+
+solve()
+    
