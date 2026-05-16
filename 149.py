8058d560b205c10fedbf8775bca94a505b122803
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-16 15:22:14 +0300

    feat: two integer problem

---
+++ 149.py
@@ -0,0 +1,16 @@
+
+
+
+def two_integer_problem():
+    t = int(input())
+
+    for _ in range(t):
+        a, b = map(int, input().split())
+
+        distance = abs(a - b)
+        total_moves = (distance + 9) // 10
+
+        print(total_moves) 
+
+
+two_integer_problem()
