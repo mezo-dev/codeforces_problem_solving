7a427a8c455f45ef61e221f77d7625765fcdfaa8
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-30 14:25:15 +0300

    feat: max profit

---
+++ 160.py
@@ -0,0 +1,29 @@
+
+
+
+def max_profit_from_project():
+    projects = [
+    ("A", 1, 100),
+    ("B", 2, 180),
+    ("C", 3, 240),
+    ("D", 4, 280),
+    ("E", 5, 300),
+    ]
+
+    hours = 8
+
+    projects.sort(key=lambda x: x[2] / x[1], reverse=True) # sort by profit per hour
+
+    selected_projects = []
+    total_profit = 0
+
+    for name, time, profit in projects:
+        if time <= hours:
+            selected_projects.append(name)
+            hours -= time
+            total_profit += profit
+    return selected_projects, total_profit
+
+
+print(max_profit_from_project())
+
