90197e1c528d3c8491b078d32a39af7721548aa2
Moataz0000 <moatazfawzy150@gmail.com>
2026-08-14 02:48:21 +0300

    feat: find word in sentance

---
+++ 185.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+def find_word_in_sentance(sentance: str, word: str):
+    for j in range(len(sentance)):
+        if sentance[j:j + len(word)] == word:
+            return f"Word is found and start at index {j}"
+    return False
+
+
+
+print(find_word_in_sentance("i love python so much", "so"))
