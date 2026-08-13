3dfce0440d24b44e5d048f4432d538a86d62f57f
Moataz0000 <moatazfawzy150@gmail.com>
2026-08-14 02:41:16 +0300

    feat: find string

--- 184.py
+++ 184.py
@@ -4,7 +4,8 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-
+        print(len(haystack) - len(needle) + 1)
+        
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i + len(needle)] == needle:
                 return i
